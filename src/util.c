b23f8a1e054fcfee619408ab15a122064f7a910c
redfox267 <create.redfox@gmail.com>
2025-01-20 22:42:25 -0500

    added outport and a struct

--- src/util.c
+++ src/util.c
@@ -7,3 +7,12 @@ void memset(void *dest, char val, uint32_t count){
     *temp++ = val;
   }
 }
+
+// outputs a byte of data to an IO port that hooked up to a PIC
+// sends data directly to PIC
+// value sent is for the initalization of those ports
+void outPortB(uint16_t port, uint8_t value) {
+  asm volatile (
+    "outb %1, %0" : : "dN" (port), "a" (value)
+  );
+}
